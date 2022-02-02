09771266ccca89f8d8498f713d4d57d59a841f95
Bill Cui <billcui2001@hotmail.com>
2022-02-02 13:41:46 -0500

    maximum-product-subarray

--- maximum-subarray/solution.md
+++ maximum-subarray/solution.md
@@ -20,8 +20,21 @@ A subarray is a contiguous part of an array.
 
 ## Solution Main Idea
 
-Divide and conquer. Get the maximum sum of left subarray (start...mid), right subarray (mid+1...end), and the subarray that passes through the midpoint.
-To find the max of subarray that passes through the midpoint it is found by finding max subarray of start...mid rooted at mid + finding max subarray of mid+1...end rooted at mid+1.
+![Intuition](maximum-subarray.png)
+
+DP. Let local_max[i] be the biggest sum out of all possible subarrays that end at index i. 
+
+local_max[i] = max(nums[i], local_max[i-1] + nums[i])
+
+in other words
+
+local_max = max(nums[i], local_max + nums[i])
+
+Get max of all local_max[i]'s to obtain answer
+
+
+
+
 
 
 
--- maximum-subarray/solution.py
+++ maximum-subarray/solution.py
@@ -1,56 +1,13 @@
-from typing import List
-
-
 class Solution:
-    def getMaxMidSubarray(self, start, end, mid, nums):
-
-        maxLeftSum = -1000000000
-        currLeftSum = 0
-        includeLeft = False
-
-        for i in reversed(range(start, mid+1)):
-            currLeftSum += nums[i]
-            if currLeftSum >= maxLeftSum:
-                includeLeft = True
-                maxLeftSum = currLeftSum
-
-        maxRightSum = -1000000000
-        currRightSum = 0
-        includeRight = False
-
-        for i in range(mid+1, end+1):
-            currRightSum += nums[i]
-            if currRightSum >= maxRightSum:
-                includeRight = True
-                maxRightSum = currRightSum
-
-        print(maxLeftSum)
-        print(maxRightSum)
-
-        result = 0
-
-        if includeLeft:
-            result += maxLeftSum
-        if includeRight:
-            result += maxRightSum
-
-        return result
-
-    def DoC(self, start, end, nums):
-        if start >= end:
-            return nums[start]
-
-        mid = (int)((start + end) / 2)
-
-        maxSubArrayLeft = self.DoC(start, mid, nums)
-        maxSubArrayRight = self.DoC(mid+1, end, nums)
-        maxSubArrayBetween = self.getMaxMidSubarray(start, end, mid, nums)
-
-
-        return max(maxSubArrayLeft, maxSubArrayRight, maxSubArrayBetween)
-
     def maxSubArray(self, nums: List[int]) -> int:
-        return self.DoC(0, len(nums)-1, nums)
-
-
-
+        
+        local_max = 0
+        
+        global_max = -inf
+        
+        for i in range(0, len(nums)):
+            local_max = max(nums[i], local_max + nums[i])
+            if local_max > global_max:
+                global_max = local_max
+        
+        return global_max
